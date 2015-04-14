99f566564b8d9d9d9cb971e572632537171760e0
Julian Steinwachs <julian.steinwachs@fau.de>
2015-04-14 07:44:10 +0200

    new example

---
+++ examples/query_timeline_dht.js
@@ -0,0 +1,19 @@
+Twisterblub = require("./Twister.js")
+
+var tschaul = Twisterblub.getUser("tschaul");
+
+Twisterblub.init({
+    host: 'http://user:pwd@127.0.0.1:28332',
+});
+
+
+
+tschaul.doFollowings(function(following){
+    
+    Twisterblub.getUser(following).doPostsSince(Date.now()/1000 - 24*60*60,function(post){
+        
+        console.log(post.getContent());
+        
+    });
+
+});
