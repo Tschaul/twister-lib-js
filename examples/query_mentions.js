afe75e87d24622f836365e4dea9ea4cab9df5ae6
Julian Steinwachs <julian.steinwachs@fau.de>
2015-05-01 16:28:26 +0200

    readme fix?

--- examples/query_mentions.js
+++ examples/query_mentions.js
@@ -1,8 +1,3 @@
 
 Twister = require("../src/Twister.js");
 
-Twister.getUser("mfreitas").doMentions(function(mentions){
-	for(var i in mentions){
-    	console.log(mentions[i].getTimestamp()+": "+mentions[i].getUsername()+": "+mentions[i].getContent());
-	}
-});
