ddc89411b447a6f2172783755c5abfb88e096416
Gabriel Esposito <gitusernameplusgithub@gmail.com>
2020-12-21 11:43:38 -0500

    +init examples for creating new user

---
+++ examples/023_create_new_account.js
@@ -0,0 +1,11 @@
+/*
+ *  TODO: document proper usage  generateClientSideAccount - createrawtransaction
+ *  TODO: add example of user to wallet - createwalletuser
+ *  TODO: add example of user pub key to twister network - endnewusertransaction
+ */
+
+Twister = require("../src/Twister.js");
+
+Twister.generateClientSideAccount ( 'foobar12' , function () {
+
+});
